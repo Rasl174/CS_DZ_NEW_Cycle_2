960d4285f6ff9a152b744018cc0187426f672b7e
Rasl174 <98830529+Rasl174@users.noreply.github.com>
2022-03-12 12:53:48 +0500

    Добавьте файлы проекта.

---
+++ CS_DZ_NEW_Cycle_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_DZ_NEW_Cycle_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int givenNumber = random.Next(2, 20);
+            int numberForTask = 2;
+            int numberExponent = 1;
+            int twoInExponent = 2;
+
+            while (givenNumber > twoInExponent)
+            {
+                twoInExponent *= numberForTask;
+                numberExponent++;
+            }
+
+            Console.WriteLine("Число из рандома - " + givenNumber);
+            Console.WriteLine("Степень - " + numberExponent);
+            Console.WriteLine("Двойка в " + numberExponent + " степени - " + twoInExponent);
+        }
+    }
+}
